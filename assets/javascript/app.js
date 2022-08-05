03a86b5c5a922ca66da5fd0d4bf779971e088917
Fernando Stadler <fernandostadler2@gmail.com>
2022-08-04 22:55:36 -0300

    Projeto finalizado

---
+++ assets/javascript/app.js
@@ -0,0 +1,35 @@
+btnNao_Prometo.addEventListener('click',()=>{
+    modal.classList.remove('hidden');
+    modal.classList.add('show');
+
+    content.classList.remove('show');
+    content.classList.add('hidden');
+})
+
+btnModal.addEventListener('click',()=>{
+    content.classList.remove('hidden');
+    content.classList.add('show');
+    
+    modal.classList.remove('show');
+    modal.classList.add('hidden');
+})
+
+btnPrometo.addEventListener('click',()=>{
+    setTimeout(()=>{
+        content_1.classList.add("show_content");
+    }, 1000)
+    setTimeout(()=>{
+        content_2.classList.add("show_content");
+    }, 2000)
+    setTimeout(()=>{
+        content_3.classList.add("show_content");
+    }, 3000)
+    msg_1.textContent = "Eu";
+    msg_2.textContent = "sempre";
+    msg_3.textContent = "estarei aqui";
+    btnGroup.classList.remove('show');
+    btnGroup.classList.add('hidden');
+    setTimeout(() => {
+        window.location.reload()
+    }, 8000);
+});
---
+++ assets/javascript/variables.js
@@ -0,0 +1,14 @@
+const btnPrometo = document.querySelector('[data-prometo]');
+const btnNao_Prometo = document.querySelector('[data-nao_prometo]');
+const btnModal = document.querySelector('[data-btn_modal]');
+const btnGroup = document.querySelector('[data-btn_group]');
+const modal = document.querySelector('[data-modal]');
+const content = document.querySelector('[data-content]');
+
+const content_1 = document.querySelector('[data-content_1]');
+const content_2 = document.querySelector('[data-content_2]');
+const content_3 = document.querySelector('[data-content_3]');
+
+const msg_1 = document.querySelector('[data-msg_1]');
+const msg_2 = document.querySelector('[data-msg_2]');
+const msg_3 = document.querySelector('[data-msg_3]');
